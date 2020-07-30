3f58cd9c05e025f0c7fa4ee2fd455456c8caae73
Linguardium <Linguardium@hobbyist.dev>
2020-07-29 17:27:28 -0700

    nashorn wins

--- src/main/java/mod/linguardium/itemnbtmodels/api/NbtMatcher.java
+++ src/main/java/mod/linguardium/itemnbtmodels/api/NbtMatcher.java
@@ -3,34 +3,27 @@ package mod.linguardium.itemnbtmodels.api;
 import com.google.gson.JsonParseException;
 import com.mojang.brigadier.StringReader;
 import com.mojang.brigadier.exceptions.CommandSyntaxException;
+import jdk.nashorn.api.scripting.NashornScriptEngineFactory;
 import net.minecraft.command.arguments.NbtPathArgumentType;
-import net.minecraft.enchantment.Enchantment;
 import net.minecraft.enchantment.EnchantmentHelper;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.Tag;
 import net.minecraft.text.Text;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
-import org.graalvm.polyglot.Context;
-import org.graalvm.polyglot.HostAccess;
-import org.graalvm.polyglot.PolyglotException;
-import org.graalvm.polyglot.Value;
-import org.graalvm.polyglot.proxy.ProxyObject;
 
+import javax.script.Bindings;
 import javax.script.ScriptEngine;
-import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import java.util.List;
-import java.util.Map;
-import java.util.function.Function;
 
 public class NbtMatcher {
-    private static Context context = Context.create("js");
-    private static ScriptEngine Engine = new ScriptEngineManager().getEngineByName("graal.js");
+    private static ScriptEngine Engine = new NashornScriptEngineFactory().getScriptEngine();
     private static NbtPathArgumentType pathArg = NbtPathArgumentType.nbtPath();
     private static String ENCHANTMENT_PATH = "";
-    static {
-        System.setProperty("polyglot.js.nashorn-compat", "true");
-    }
+    //static {
+    //    System.setProperty("polyglot.js.nashorn-compat", "true");
+    //}
     static public class NbtCheckValue {
         String eval;
         NbtCheckValue(String eval) {
@@ -43,7 +36,6 @@ public class NbtMatcher {
         StackMatcher(ItemStack itemStack) {
             stack = itemStack;
         }
-        @HostAccess.Export
         public Long Number(String string) {
             Long l = 0L;
             try {
@@ -51,7 +43,6 @@ public class NbtMatcher {
             }catch(NumberFormatException ignored) { }
             return l;
         }
-        @HostAccess.Export
         public Double Decimal(String string) {
             Double d = 0.0D;
             try {
@@ -60,11 +51,9 @@ public class NbtMatcher {
             return d;
 
         }
-        @HostAccess.Export
         public String String(String string) {
             return path(string);
         }
-        @HostAccess.Export
         public String Text(String string) {
             Text t=null;
             try {
@@ -74,7 +63,6 @@ public class NbtMatcher {
                 return t.asString();
             return "";
         }
-        @HostAccess.Export
         public Integer Enchantment(String string) {
                 return EnchantmentHelper.getLevel(Registry.ENCHANTMENT.get(new Identifier(string)),stack);
         }
@@ -91,13 +79,16 @@ public class NbtMatcher {
 
     }
     public static boolean matches(ItemStack stack, NbtCheckValue pred) {
-        Value bindings = context.getBindings("js");
-        bindings.putMember("Nbt",new StackMatcher(stack.copy()));
+        Bindings bindings = Engine.createBindings();
+        bindings.put("Nbt",new StackMatcher(stack.copy()));
+        //Value bindings = context.getBindings("js");
+        //bindings.putMember("Nbt",new StackMatcher(stack.copy()));
         try {
-            Value retval = context.eval("js", pred.eval);
-            if (retval.isBoolean())
-                return retval.asBoolean();
-        }catch(PolyglotException ignored) { }
+            Object retval = Engine.eval(pred.eval,bindings);
+            //Value retval = context.eval("js", pred.eval);
+            if (retval instanceof Boolean)
+                return (Boolean)retval;
+        }catch(ScriptException ignored) { }
         return false;
     }
 }
